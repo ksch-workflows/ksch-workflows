37b1d393547e60fa442f118e46eade6d308aa1de
Jan Mewes <jan.mewes@ks-plus.org>
2018-11-03 13:35:26 +0100

    Fix unit test

--- user-interface/src/test/java/util/TimeTest.java
+++ user-interface/src/test/java/util/TimeTest.java
@@ -19,6 +19,17 @@ public class TimeTest {
         assertEquals("Failed to parse day from date", 27, parsedDate.getDayOfMonth());
     }
 
+    @Test
+    public void should_parse_date_with_single_digit_month_and_day() {
+        String date = "1-1-2000";
+
+        LocalDate parsedDate = Time.parseDate(date);
+
+        assertEquals("Failed to parse year from date", 2000, parsedDate.getYear());
+        assertEquals("Failed to parse month from date", 1, parsedDate.getMonth().getValue());
+        assertEquals("Failed to parse day from date", 1, parsedDate.getDayOfMonth());
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void should_not_accept_invalid_date_format() {
         String date = "27/07/2000";
