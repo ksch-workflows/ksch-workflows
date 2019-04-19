1d6b106ceb39c7d9a31b54cdf0417d5966c28545
Jan Mewes <jan.mewes@ks-plus.org>
2019-04-19 17:08:40 +0200

    WIP

--- user-interface/src/main/resources/static/ksch/registration/PatientFormFields.js
+++ user-interface/src/main/resources/static/ksch/registration/PatientFormFields.js
@@ -1,15 +1,23 @@
 
 var YEAR_EPOCH = 1970;
 
-function age(dateOfBirth, today = new Date()) {
+function age( dateOfBirth, today = new Date() ) {
     var lifeTimeInMillis = today.getTime() - dateOfBirth.getTime();
     return Math.abs(new Date(lifeTimeInMillis).getUTCFullYear() - YEAR_EPOCH);
 }
 
-function date(year, month, day) {
+function date( year, month, day ) {
     return new Date(year + "-" + month + "-" + day);
 }
 
 function onAgeChanged() {
 
+}
+
+function getDateOfBirth( inputId ) {
+    var retrievedDateOfBirth = document.getElementById( inputId ).value
+    if ( ! new RegExp("[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}").test(retrievedDateOfBirth) ) {
+        return null;
+    }
+    return new Date( retrievedDateOfBirth );
 }
--- user-interface/src/main/resources/static/ksch/registration/PatientFormFields.test.js
+++ user-interface/src/main/resources/static/ksch/registration/PatientFormFields.test.js
@@ -10,3 +10,21 @@ QUnit.test( "Should create date object", function( assert ) {
 QUnit.test( "xxx", function( assert ) {
     assert.ok( 1, "Passed!" );
 });
+
+
+// update age
+
+QUnit.test( "Should read in date of birth", function( assert ) {
+    var dateOfBirth = getDateOfBirth("dateOfBirth");
+    assert.equal( dateOfBirth.getTime(), new Date("2005-11-25").getTime() );
+});
+
+QUnit.test( "Should skip empty date of birth", function( assert ) {
+    var dateOfBirth = getDateOfBirth("dateOfBirth_empty");
+    assert.notOk( dateOfBirth );
+});
+
+QUnit.test( "Should skip incomplete date of birth", function( assert ) {
+    var dateOfBirth = getDateOfBirth("dateOfBirth_incomplete");
+    assert.notOk( dateOfBirth );
+});
