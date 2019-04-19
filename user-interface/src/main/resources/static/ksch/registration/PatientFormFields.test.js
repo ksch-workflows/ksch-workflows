8a3e0e0a6a934c0ea8679a21e01e160358c85f39
Jan Mewes <jan.mewes@ks-plus.org>
2019-04-19 12:19:34 +0200

    Add age to patient form

--- user-interface/src/main/resources/static/ksch/registration/PatientFormFields.test.js
+++ user-interface/src/main/resources/static/ksch/registration/PatientFormFields.test.js
@@ -5,6 +5,10 @@ QUnit.test( "Should calculate age from date of birth", function( assert ) {
   assert.equal( module.age(new Date("2000-01-01"), new Date("2019-01-01")), 19 );
 });
 
+QUnit.test( "Should create date object", function( assert ) {
+  assert.equal( module.age(new Date("2000-01-01"), new Date("2019-01-01")), 19 );
+});
+
 QUnit.test( "xxx", function( assert ) {
   assert.ok( 1, "Passed!" );
 });
