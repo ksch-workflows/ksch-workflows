0ab3a5306c9971b1ceabbb556faad8e39daba12d
Jan Mewes <jan.mewes@ks-plus.org>
2019-04-19 12:29:22 +0200

    Add factory function for date

--- user-interface/src/main/resources/static/ksch/registration/PatientFormFields.js
+++ user-interface/src/main/resources/static/ksch/registration/PatientFormFields.js
@@ -6,6 +6,11 @@ function age(dateOfBirth, today = new Date()) {
     return Math.abs(new Date(lifeTimeInMillis).getUTCFullYear() - YEAR_EPOCH);
 }
 
+function date(year, month, day) {
+    return new Date(year + "-" + month + "-" + day);
+}
+
 if (typeof module !== 'undefined' && module.exports) {
     exports.age = age;
+    exports.date = date;
 }
