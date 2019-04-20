3a9759bea845e8bf22d115ffa1bad08b6f9f6196
Jan Mewes <jan.mewes@ks-plus.org>
2019-04-20 15:00:55 +0200

    WIP

--- user-interface/src/main/resources/static/ksch/registration/PatientFormFields.test.js
+++ user-interface/src/main/resources/static/ksch/registration/PatientFormFields.test.js
@@ -1,9 +1,17 @@
 
-QUnit.test( "Should calculate age from date of birth", function( assert ) {
+// -----------------------------------------------------------------------------
+QUnit.module("calculateAge");
+// -----------------------------------------------------------------------------
+
+QUnit.test("Should calculate age from date of birth", function(assert) {
     assert.equal(calculateAge(new Date("2000-01-01"), new Date("2019-01-01")), 19);
 });
 
-QUnit.test( "Should read in date of birth", function( assert ) {
+// -----------------------------------------------------------------------------
+QUnit.module("getDateOfBirth");
+// -----------------------------------------------------------------------------
+
+QUnit.test("Should read in date of birth", function(assert) {
     var expectedDateOfBirth = new Date("2005-11-25");
     expectedDateOfBirth.setHours(0);
 
@@ -12,17 +20,21 @@ QUnit.test( "Should read in date of birth", function( assert ) {
     assert.equal(dateOfBirth.getTime(), expectedDateOfBirth.getTime());
 });
 
-QUnit.test( "Should skip empty date of birth", function( assert ) {
+QUnit.test("Should skip empty date of birth", function(assert) {
     var dateOfBirth = getDateOfBirth("dateOfBirth_empty");
     assert.notOk(dateOfBirth);
 });
 
-QUnit.test( "Should skip incomplete date of birth", function( assert ) {
+QUnit.test("Should skip incomplete date of birth", function(assert) {
     var dateOfBirth = getDateOfBirth("dateOfBirth_incomplete");
     assert.notOk(dateOfBirth);
 });
 
-QUnit.test( "Should set date of birth input field", function( assert ) {
+// -----------------------------------------------------------------------------
+QUnit.module("setDateOfBirth");
+// -----------------------------------------------------------------------------
+
+QUnit.test("Should set date of birth input field", function(assert) {
     var dateOfBirth = new Date("2005-11-25");
     dateOfBirth.setHours(0);
 
@@ -31,7 +43,11 @@ QUnit.test( "Should set date of birth input field", function( assert ) {
     assert.equal(getDateOfBirth("dateOfBirth_testSetValue").getTime(), dateOfBirth.getTime());
 });
 
-QUnit.test( "Should read in age", function( assert ) {
+// -----------------------------------------------------------------------------
+QUnit.module("getAge");
+// -----------------------------------------------------------------------------
+
+QUnit.test("Should read in age", function(assert) {
     var expectedAge = 25;
 
     var retrievedAge = getAge("age_withInitialValue");
@@ -39,7 +55,11 @@ QUnit.test( "Should read in age", function( assert ) {
     assert.equal(retrievedAge, expectedAge);
 });
 
-QUnit.test( "Should set age", function( assert ) {
+// -----------------------------------------------------------------------------
+QUnit.module("setAge");
+// -----------------------------------------------------------------------------
+
+QUnit.test("Should set age", function(assert) {
     setAge(15, "age_withoutValue");
     assert.equal(getAge("age_withoutValue"), 15);
 });
