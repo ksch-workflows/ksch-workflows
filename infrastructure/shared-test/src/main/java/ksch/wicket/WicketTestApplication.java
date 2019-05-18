77390b39032485b3e97747bd4a50deb89b0c6c84
Jan Mewes <jan.mewes@ks-plus.org>
2019-05-18 17:57:29 +0200

    Apply checkstyle remarks

--- infrastructure/shared-test/src/main/java/ksch/wicket/WicketTestApplication.java
+++ infrastructure/shared-test/src/main/java/ksch/wicket/WicketTestApplication.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package ksch.wicket;
 
 import org.apache.wicket.Page;
--- user-interface/src/main/java/ksch/Activity.java
+++ user-interface/src/main/java/ksch/Activity.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package ksch;
 
 import org.apache.wicket.markup.html.basic.Label;
