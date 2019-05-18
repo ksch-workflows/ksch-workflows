63bf15ba34b465383a1eb5dc290f547e4335ff62
Jan Mewes <jan.mewes@ks-plus.org>
2019-05-18 17:57:29 +0200

    Apply checkstyle remarks

--- infrastructure/shared-test/src/main/java/ksch/util/Debug.java
+++ infrastructure/shared-test/src/main/java/ksch/util/Debug.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package ksch.util;
 
 import org.apache.wicket.util.tester.WicketTester;
--- patient-management/patient-management-impl/src/main/java/ksch/patientmanagement/visit/VisitRepository.java
+++ patient-management/patient-management-impl/src/main/java/ksch/patientmanagement/visit/VisitRepository.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package ksch.patientmanagement.visit;
 
 import org.springframework.data.jpa.repository.Query;
