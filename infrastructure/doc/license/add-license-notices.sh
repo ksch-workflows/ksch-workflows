0ffdd38d2d1776cd60201afc3079f39f541a60ed
Jan Mewes <jan.mewes@ks-plus.org>
2019-05-18 17:09:45 +0200

    Improve util script for license headers

--- infrastructure/doc/license/add-license-notices.sh
+++ infrastructure/doc/license/add-license-notices.sh
@@ -6,9 +6,16 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_DIR=$SCRIPT_DIR/../../..
 
-for i in $(find $PROJECT_DIR -name "*.html")
+for i in $(find $PROJECT_DIR -name "*.html" -not -path "*/fileTemplates*")
 do
     if ! grep -q Copyright $i ; then
         cat $SCRIPT_DIR/license-notice-html.txt $i >$i.new && mv $i.new $i
+    fi
+done
+
+for i in $(find $PROJECT_DIR -name "*.java" -not -path "*/fileTemplates*")
+do
+    if ! grep -q Copyright $i ; then
+        cat $SCRIPT_DIR/license-notice-java.txt $i >$i.new && mv $i.new $i
     fi
 done
