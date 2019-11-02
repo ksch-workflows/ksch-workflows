7d42a624cfaf79ec3f1db8dd91a4119c88ca44eb
Jan Mewes <jan.mewes@ks-plus.org>
2019-11-02 18:48:48 +0100

    WIP  [ci skip]

--- user-interface/src/test/java/ksch/registration/EditPatientDetailsActivityTest.java
+++ user-interface/src/test/java/ksch/registration/EditPatientDetailsActivityTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 KS-plus e.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ksch.registration;
 
 import ksch.patientmanagement.patient.PatientQueries;
@@ -9,6 +25,7 @@ import ksch.wicket.MockBean;
 import ksch.wicket.PageComponentTest;
 import org.junit.Test;
 
+import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
@@ -34,5 +51,7 @@ public class EditPatientDetailsActivityTest extends PageComponentTest {
         var editPatientDetailsActivity = new EditPatientDetailsActivity(patient.getId());
 
         tester.startComponentInPage(editPatientDetailsActivity);
+        
+        fail();
     }
 }
